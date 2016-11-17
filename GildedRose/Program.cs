d9ba40f2d2ed6c814eb215274a0804b109bedf9b
Victor Grigoriu <vgrigoriu@gmail.com>
2016-11-17 15:24:10 +0200

    tabs -> spaces, comment conjured items

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -33,8 +33,8 @@ namespace GildedRose
                     SellIn = 5,
                     Quality = 49
                 },
-				// this conjured item does not work properly yet
-				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+                // this conjured item does not work properly yet
+                //new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
             };
 
             var app = new GildedRose(Items);

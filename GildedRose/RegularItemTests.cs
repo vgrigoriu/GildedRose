ba20e5071b92fe023c3e6fcc2dc4ec94a7418141
Victor Grigoriu <vgrigoriu@gmail.com>
2016-11-21 16:55:35 +0200

    Test normal update on regular items

---
+++ GildedRose/RegularItemTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace GildedRose
+{
+    [TestFixture]
+    public class RegularItemTests
+    {
+        [Test]
+        public void SellInDecreasesByOne()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.AreEqual(9, items[0].SellIn);
+        }
+
+        [Test]
+        public void QualityDecreasesByOne()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.AreEqual(9, items[0].Quality);
+        }
+    }
+}

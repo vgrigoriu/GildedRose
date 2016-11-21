2cf04c4004f5c8bc55bce0b391e9426f8203edb7
Victor Grigoriu <vgrigoriu@gmail.com>
2016-11-21 17:00:26 +0200

    Once the sell by date has passed, `Quality` degrades twice as fast

--- GildedRose/RegularItemTests.cs
+++ GildedRose/RegularItemTests.cs
@@ -23,5 +23,19 @@ namespace GildedRose
             app.UpdateQuality();
             Assert.AreEqual(9, items[0].Quality);
         }
+
+        [Test]
+        public void QualityDecreasesByTwoAfterSellByDate()
+        {
+            IList<Item> items = new List<Item>
+            {
+                new Item { Name = "foo", SellIn = 0, Quality = 10 },
+                new Item { Name = "foo", SellIn = -3, Quality = 10 },
+            };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.AreEqual(8, items[0].Quality, "SellBy == 0");
+            Assert.AreEqual(8, items[1].Quality, "Negative SellBy value");
+        }
     }
 }

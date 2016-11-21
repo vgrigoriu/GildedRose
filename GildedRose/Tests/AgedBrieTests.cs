66b7ccc69d8c3cfe2d4dffe5164b80322dea1bde
Victor Grigoriu <vgrigoriu@gmail.com>
2016-11-21 17:15:56 +0200

    Expired Aged Brie

--- GildedRose/Tests/AgedBrieTests.cs
+++ GildedRose/Tests/AgedBrieTests.cs
@@ -14,5 +14,19 @@ namespace GildedRose.Tests
             app.UpdateQuality();
             Assert.AreEqual(11, items[0].Quality);
         }
+
+        [Test]
+        public void AgedBrieQualityIncreasesByTwoAfterSellByDate()
+        {
+            IList<Item> items = new List<Item>
+            {
+                new Item { Name = "Aged Brie", SellIn = 0, Quality = 10 },
+                new Item { Name = "Aged Brie", SellIn = -3, Quality = 10 },
+            };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.AreEqual(12, items[0].Quality, "SellBy == 0");
+            Assert.AreEqual(12, items[1].Quality, "Negative SellBy value");
+        }
     }
 }

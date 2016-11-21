9b7142ef9cfded11479a1048a1ab711cfa2d4104
Victor Grigoriu <vgrigoriu@gmail.com>
2016-11-21 17:03:42 +0200

    The `Quality` of an item is never negative

--- GildedRose/RegularItemTests.cs
+++ GildedRose/RegularItemTests.cs
@@ -37,5 +37,19 @@ namespace GildedRose
             Assert.AreEqual(8, items[0].Quality, "SellBy == 0");
             Assert.AreEqual(8, items[1].Quality, "Negative SellBy value");
         }
+
+        [Test]
+        public void QualityIsNeverNegative()
+        {
+            IList<Item> items = new List<Item>
+            {
+                new Item { Name = "normal item at 0 quality", SellIn = 10, Quality = 0 },
+                new Item { Name = "expired item at 1 quality", SellIn = 0, Quality = 1 }
+            };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.AreEqual(0, items[0].Quality, items[0].Name);
+            Assert.AreEqual(0, items[1].Quality, items[1].Name);
+        }
     }
 }

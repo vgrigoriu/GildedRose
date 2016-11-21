5cc6d38c53e037d57993570a313ed00677a51569
Victor Grigoriu <vgrigoriu@gmail.com>
2016-11-21 17:10:43 +0200

    _Aged Brie_ actually increases in `Quality` the older it gets

---
+++ GildedRose/Tests/AgedBrieTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace GildedRose.Tests
+{
+    [TestFixture]
+    public class AgedBrieTests
+    {
+        [Test]
+        public void AgebBrieIncreasesInQuality()
+        {
+            IList<Item> items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 10, Quality = 10 } };
+            GildedRose app = new GildedRose(items);
+            app.UpdateQuality();
+            Assert.AreEqual(11, items[0].Quality);
+        }
+    }
+}
